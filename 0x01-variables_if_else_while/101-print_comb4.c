13f362a1e3403aa9eaaa090178549de9f93b281c
FarEl007 <tomiwaadesiyan@gmail.com>
2022-04-10 14:57:31 -0700

    Fixed the mistakes

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of three different digits, in ascending order, separated by a comma followed by a space.
+ * main - Prints all possible combinations of three different digits, 
+ * in ascending order, separated by a comma followed by a space.
  *
  * Return: Always 0.
  */
@@ -19,16 +20,16 @@ putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
 putchar((digit3 % 10) + '0');
 
-if (digit1 == 7 && digit2 == 8 && digit3 ==9)
+if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 continue;
 
 putchar(',');
-putchar(',');
+putchar(' ');
 }
 }
 }
 
 putchar('\n');
 
-return (0)
+return (0);
 }
