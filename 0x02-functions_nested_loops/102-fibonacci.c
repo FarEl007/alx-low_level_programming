12b98da6b847911a9f1c5a4ee67e398cba9790ce
FarEl007 <tomiwaadesiyan@gmail.com>
2022-04-12 21:05:56 -0700

    Corrected all errors

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,23 +11,22 @@ int main(void)
 int i = 0;
 
 long int a = 0, b = 1, next;
- 
-while (i < 50)    
+
+while (i < 50)
 {
-next = a + b;      
-a = b;      
+next = a + b;
+a = b;
 b = next;
-      
 printf("%lu", next);
-            
+
 if (i < 49)
-{	  
+{
 printf(", ");
-	  
-}      
+
+}
 i++;
 
-}  
+}
 putchar('\n');
 return (0);
 }
